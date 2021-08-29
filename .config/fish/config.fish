523affbfe0f6558fe00e618dfcfe7133d263936d
daniel <danichau93@gmail.com>
2021-08-28 18:38:03 -0700

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,10 @@ set -x GTK_IM_MODULE fcitx
 set -x QT_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
 
-set -x PATH $PATH $HOME/.local/bin
-set -x PATH $PATH $HOME/.yarn/bin
+set -px PATH $HOME/.local/bin
+set -px PATH $HOME/.yarn/bin
 
 kitty + complete setup fish | source
-register-python-argcomplete --shell fish pubs | source
 
 if test -z $DISPLAY; and [ (tty) = /dev/tty1 ];
   set -x MOZ_ENABLE_WAYLAND 1
