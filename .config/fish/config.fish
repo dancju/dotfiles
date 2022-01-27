db07835642122cb7f98e55dd3f53396b5ceaef99
daniel <danichau93@gmail.com>
2022-01-27 17:44:56 -0600

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,15 +6,9 @@ set -x XMODIFIERS @im=fcitx
 set -px PATH $HOME/.local/bin
 set -px PATH $HOME/.yarn/bin
 
-kitty + complete setup fish | source
-
 if test -z $DISPLAY; and [ (tty) = /dev/tty1 ];
   set -x MOZ_ENABLE_WAYLAND 1
-  set -x QT_QPA_PLATFORM wayland
+  set -x QT_QPA_PLATFORM "wayland;xcb"
   set -x XDG_SESSION_TYPE wayland
-  sway
-end
-
-if test -z $DISPLAY; and [ (tty) = /dev/tty2 ];
-  startx
+  exec sway
 end
